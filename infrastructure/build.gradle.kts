9be9f22c6bb5a096165b928733db1baabfafb8ed
jayon <basejin0810@naver.com>
2023-01-17 21:48:58 +0900

    feat: add test dependencies in infra

--- infrastructure/build.gradle.kts
+++ infrastructure/build.gradle.kts
@@ -1,6 +1,13 @@
 dependencies {
     implementation(project(":domain"))
 
+    /* database */
     implementation("com.mysql:mysql-connector-j")
     implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
+
+    /* testing */
+    testImplementation("io.projectreactor:reactor-test")
+    testImplementation("io.rest-assured:rest-assured")
+    testImplementation("io.rest-assured:json-path")
+    testImplementation("io.rest-assured:kotlin-extensions")
 }
