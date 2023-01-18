bd75d1c967e0bcfc12bf8b1f0b9ac8e20717e20f
jayon <basejin0810@naver.com>
2023-01-19 00:04:14 +0900

    feat: add test environment in all layer

--- application/build.gradle.kts
+++ application/build.gradle.kts
@@ -1,4 +1,15 @@
 dependencies {
     implementation(project(":domain"))
     implementation(project(":infrastructure"))
+
+    /* kotlin */
+    implementation(kotlin("stdlib-jdk8"))
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+
+    /* testing */
+    testImplementation("io.github.microutils:kotlin-logging-jvm:3.0.4")
+    testImplementation("io.projectreactor:reactor-test")
+    testImplementation("io.kotest:kotest-runner-junit5:5.5.4")
+    testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.2")
 }
--- infrastructure/build.gradle.kts
+++ infrastructure/build.gradle.kts
@@ -1,13 +1,26 @@
 dependencies {
     implementation(project(":domain"))
+    testImplementation(project(":domain"))
+
+    /* kotlin */
+    implementation(kotlin("stdlib-jdk8"))
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
 
     /* database */
-    implementation("com.mysql:mysql-connector-j")
+    implementation("mysql:mysql-connector-java")
+    implementation("dev.miku:r2dbc-mysql:0.8.2.RELEASE")
     implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
+    implementation("org.flywaydb:flyway-core")
+    implementation("org.flywaydb:flyway-mysql:9.11.0")
 
     /* testing */
+    testImplementation("io.github.microutils:kotlin-logging-jvm:3.0.4")
     testImplementation("io.projectreactor:reactor-test")
-    testImplementation("io.rest-assured:rest-assured")
-    testImplementation("io.rest-assured:json-path")
-    testImplementation("io.rest-assured:kotlin-extensions")
+    testImplementation("io.kotest:kotest-runner-junit5:5.5.4")
+    testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.2")
+    testImplementation("org.testcontainers:testcontainers:1.17.6")
+    testImplementation("org.testcontainers:mysql:1.17.6")
+    testImplementation("org.testcontainers:r2dbc:1.17.6")
+    testImplementation("org.testcontainers:junit-jupiter:1.17.6")
 }
