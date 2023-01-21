b3cd2c5ba97f25ad6e20e12beea6458f835b2ef8
jayon <basejin0810@naver.com>
2023-01-21 22:23:29 +0900

    refactor isNewRow 정의 방식 수정

--- infrastructure/src/main/kotlin/com/jvmhater/moduticket/model/ModuRow.kt
+++ infrastructure/src/main/kotlin/com/jvmhater/moduticket/model/ModuRow.kt
@@ -1,25 +1,21 @@
 package com.jvmhater.moduticket.model
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.data.annotation.Id
 import org.springframework.data.annotation.Transient
 import org.springframework.data.relational.core.mapping.Column
 import org.springframework.data.relational.core.mapping.Table
 
 @Table("modu")
-data class ModuRow(@Id @Column("id") override val rowId: String, val name: String) : Row<String> {
-    @Transient
-    override var isNewRow: Boolean = true
-        private set
-
-    constructor(rowId: String, name: String, flag: Boolean) : this(rowId, name) {
-        isNewRow = flag
-    }
+class ModuRow(
+    @Id @Column("id") override val rowId: String,
+    val name: String,
+    @Transient @Value("null") override val isNewRow: Boolean = false,
+) : Row<String> {
 
     fun toDomain(): Modu = Modu(id = id, name = name)
-
 }
 
 fun Modu.toRow(isNewRow: Boolean = false): ModuRow {
-    val row = ModuRow(rowId = id, name = name, flag = isNewRow)
-    return row
+    return ModuRow(rowId = id, name = name, isNewRow = isNewRow)
 }
