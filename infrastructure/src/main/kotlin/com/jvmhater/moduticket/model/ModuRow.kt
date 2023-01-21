4b41aff2f677d36965de301b716616d5a51333f6
youjmen <youjmen15@gmail.com>
2023-01-21 19:48:34 +0900

    fix: make modurow secondary constructor

--- infrastructure/src/main/kotlin/com/jvmhater/moduticket/model/ModuRow.kt
+++ infrastructure/src/main/kotlin/com/jvmhater/moduticket/model/ModuRow.kt
@@ -6,17 +6,20 @@ import org.springframework.data.relational.core.mapping.Column
 import org.springframework.data.relational.core.mapping.Table
 
 @Table("modu")
-class ModuRow(@Id @Column("id") override val rowId: String, val name: String) : Row<String> {
-
+data class ModuRow(@Id @Column("id") override val rowId: String, val name: String) : Row<String> {
     @Transient
-    override var isNewRow: Boolean = false
-        internal set
+    override var isNewRow: Boolean = true
+        private set
+
+    constructor(rowId: String, name: String, flag: Boolean) : this(rowId, name) {
+        isNewRow = flag
+    }
 
     fun toDomain(): Modu = Modu(id = id, name = name)
+
 }
 
 fun Modu.toRow(isNewRow: Boolean = false): ModuRow {
-    val row = ModuRow(rowId = id, name = name)
-    row.isNewRow = isNewRow
+    val row = ModuRow(rowId = id, name = name, flag = isNewRow)
     return row
 }
