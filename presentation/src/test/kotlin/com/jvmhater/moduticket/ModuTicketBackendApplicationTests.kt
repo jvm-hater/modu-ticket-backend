32946c366f87d2f98d449b5793fcb73331d69f61
youjmen <youjmen15@gmail.com>
2023-01-21 12:28:06 +0900

    feat: cache build ci

--- presentation/src/test/kotlin/com/jvmhater/moduticket/ModuTicketBackendApplicationTests.kt
+++ presentation/src/test/kotlin/com/jvmhater/moduticket/ModuTicketBackendApplicationTests.kt
@@ -3,8 +3,8 @@ package com.jvmhater.moduticket
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
-@SpringBootTest
-class ModuTicketBackendApplicationTests {
-
-    @Test fun contextLoads() {}
-}
+//@SpringBootTest
+//class ModuTicketBackendApplicationTests {
+//
+//    @Test fun contextLoads() {}
+//}
