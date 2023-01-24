b2b48f23e7fa5758da78afb1e82203a7e9c22645
jayon <basejin0810@naver.com>
2023-01-24 12:36:42 +0900

    feat: r2dbcEntityTemplate 추가

--- infrastructure/src/test/kotlin/com/jvmhater/moduticket/repository/SpringDataModuRepositoryTest.kt
+++ infrastructure/src/test/kotlin/com/jvmhater/moduticket/repository/SpringDataModuRepositoryTest.kt
@@ -5,16 +5,19 @@ import com.jvmhater.moduticket.readResourceFile
 import com.jvmhater.moduticket.testcontainers.TestMySQLContainer
 import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.matchers.shouldBe
+import org.springframework.data.r2dbc.core.R2dbcEntityTemplate
+import org.springframework.data.r2dbc.core.ReactiveSelectOperation.SelectWithQuery
 import org.springframework.r2dbc.core.DatabaseClient
 import org.springframework.test.context.ContextConfiguration
 
 @ContextConfiguration(classes = [TestR2bcConfiguration::class])
 class SpringDataModuRepositoryTest(
     databaseClient: DatabaseClient,
+    r2dbcEntityTemplate: R2dbcEntityTemplate,
     moduR2dbcRepository: ModuR2dbcRepository,
 ) : DescribeSpec({ afterEach { TestMySQLContainer.sql(readResourceFile("ddl/truncate.sql")) } }) {
 
-    private val moduRepositoryImpl = ModuRepositoryImpl(databaseClient, moduR2dbcRepository)
+    private val moduRepositoryImpl = ModuRepositoryImpl(databaseClient, r2dbcEntityTemplate, moduR2dbcRepository)
 
     init {
         describe("#save") {
