d0e7e3b4fc5c06deb6fe091d79812448c04c5562
jaemin <youjmen15@gmail.com>
2023-02-19 18:44:04 +0900

    fix: test

--- infrastructure/src/main/kotlin/com/jvmhater/moduticket/repository/SpringDataUserRepository.kt
+++ infrastructure/src/main/kotlin/com/jvmhater/moduticket/repository/SpringDataUserRepository.kt
@@ -6,8 +6,8 @@ import com.jvmhater.moduticket.model.UserRow
 import com.jvmhater.moduticket.model.toDomains
 import kotlinx.coroutines.flow.toList
 import org.springframework.dao.DataAccessException
-import org.springframework.dao.DataIntegrityViolationException
 import org.springframework.data.repository.kotlin.CoroutineCrudRepository
+import org.springframework.r2dbc.UncategorizedR2dbcException
 import org.springframework.stereotype.Repository
 
 @Repository
@@ -17,19 +17,19 @@ class SpringDataUserRepository(
     private val r2dbcIssuedCouponRepository: R2dbcIssuedCouponRepository
 ) : UserRepository {
 
-    override suspend fun create(id: String, password: String) {
+    override suspend fun create(userId: String, password: String) {
         try {
-            r2dbcUserRepository.save(UserRow(isNewRow = true, userId = id, password = password))
-        } catch (e: DataIntegrityViolationException) {
+            r2dbcUserRepository.save(UserRow(isNewRow = true, userId = userId, password = password))
+        } catch (e: UncategorizedR2dbcException) {
             throw RepositoryException.RecordAlreadyExisted(e, "이미 존재하는 유저입니다.")
         } catch (e: DataAccessException) {
             throw RepositoryException.UnknownAccessFailure(e, "데이터베이스 연결에 실패하였습니다.")
         }
     }
 
-    override suspend fun find(id: String): User {
+    override suspend fun find(userId: String): User {
         try {
-            return r2dbcUserRepository.findByUserId(id).toDomain()
+            return r2dbcUserRepository.findByUserId(userId).toDomain()
         } catch (e: Exception) {
             throw RepositoryException.RecordNotFound(message = "존재하지 않는 유저입니다.")
         }
@@ -39,11 +39,11 @@ class SpringDataUserRepository(
         return r2dbcUserRepository.findAll().toList().map { it.toDomain() }
     }
 
-    override suspend fun findWithIssuedCoupon(id: String): User {
+    override suspend fun findWithIssuedCoupon(userId: String): User {
         try {
-            val user = find(id)
+            val user = find(userId)
             val coupons =
-                r2dbcCouponRepository.findCouponJoinIssuedCouponByUserId(id).toList().toDomains()
+                r2dbcCouponRepository.findCouponJoinIssuedCouponByUserId(userId).toList().toDomains()
 
             user.addCoupons(coupons)
             return user
@@ -52,11 +52,11 @@ class SpringDataUserRepository(
         }
     }
 
-    override suspend fun delete(id: String) {
-        r2dbcUserRepository.findById(id)
+    override suspend fun delete(userId: String) {
+        r2dbcUserRepository.findByUserId(userId)
             ?: throw RepositoryException.RecordNotFound(message = "존재하지 않는 유저입니다.")
-        r2dbcUserRepository.deleteById(id)
-        r2dbcIssuedCouponRepository.deleteAllByUserId(id)
+        r2dbcUserRepository.findByUserId(userId)
+        r2dbcIssuedCouponRepository.deleteAllByUserId(userId)
     }
 }
 
